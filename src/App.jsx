b5725b097f1cd5bf37ec63c5beec635f4729359e
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-11 18:48:07 +0100

    skills

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
    <div>
-    <H1/>
+       <H1/>
    </div>
   )
 }
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+      <div>
+        <h2>FOLARIN ABIOLA</h2>
+      </div>
+
+      <div>
+        <h2>OPEN TO WORK</h2>
+        <h2>TOMORROW</h2>
+      </div>
+
+
+      <div>
+        <h2>CONTACT</h2>
+      </div>
+    </>
+  )
+}
+
+export default Nav
