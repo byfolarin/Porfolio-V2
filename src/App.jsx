c039a5fd06a8155ba6f07dfe0debecd15ee2f7ff
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-11 18:18:04 +0100

    git-value

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App() {
 
 
   return (
-    <div className='w-full h-full '>
-      <div className='flex justify-center items-center'>
-        <h1 className='text-[#F13C00] text-[260px] tracking-[-0.11em] mt-32 leading-8'>ENGINEER</h1>
+    <div>
+      <div>
+        <h1 className='text-[#F13C00] text-[260px] tracking-[-0.11em]'>ENGINEER</h1>
      </div>
     </div>
   )
