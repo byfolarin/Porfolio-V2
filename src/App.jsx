e4020f4d5563b3e712aa714f5e5f098a684a0db3
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-11 21:53:48 +0100

    overflow

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,14 @@ import Nav from './Components/Nav'
 import ImgChange from './Components/ImgChange'
 
 function App() {
-
-
   return (
-   <div className='w-screen h-screen overflow-hidden mx-11'>
-
- 
-    <Nav />
-    <ImgChange />
-    <H1/>
-   </div>
+    <div className="h-screen flex flex-col justify-between mx-11">
+      <Nav />
+      <div className="flex-grow">
+        <ImgChange />
+      </div>
+      <H1 />
+    </div>
   )
 }
 
--- src/Components/H1.jsx
+++ src/Components/H1.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const H1 = () => {
   return (
-    <div className="min-h-screen flex items-end justify-center p-4">
-      <div>
-        <h1 className='text-[#F13C00] text-[15vw] sm:text-[20vw] md:text-[25vw] lg:text-[280px] tracking-[-0.12em] leading-[0.8]'>
+    <div className="h-screen flex items-end justify-center overflow-hidden">
+      <div className="mb-8 w-full px-4"> {/* Added full width and horizontal padding */}
+        <h1 className='text-[#F13C00] text-[15vw] 
+        sm:text-[20vw] md:text-[25vw] lg:text-[280px] 
+        tracking-[-0.12em] leading-[0.8] text-center
+        break-words'> {/* Added text-center and break-words */}
           ENGINEER
         </h1>
       </div>
