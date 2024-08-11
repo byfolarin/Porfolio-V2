47e365589aa5d5a40a07c071a042c7c34c35983a
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-11 19:16:40 +0100

    projects-comp

--- src/Components/H1.jsx
+++ src/Components/H1.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 
 const H1 = () => {
   return (
-
-    <div>
-       <div className="h-screen flex items-end justify-center">
-          <div>
-            <h1 className='text-[#F13C00] text-[280px] tracking-[-0.12em]'>ENGINEER</h1>
-          </div>
-       </div>
+    <div className="min-h-screen flex items-end justify-center p-4">
+      <div>
+        <h1 className='text-[#F13C00] text-[15vw] sm:text-[20vw] md:text-[25vw] lg:text-[280px] tracking-[-0.12em] leading-[0.8]'>
+          ENGINEER
+        </h1>
+      </div>
     </div>
-    
   )
 }
 
--- src/Components/ImgChange.jsx
+++ src/Components/ImgChange.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ImgChange = () => {
   return (
-    <div className='flex items-start justify-center gap-3'>
-      <div className='w-[80px] h-[87px] bg-slate-200'></div>
-      <div className='w-[80px] h-[80px] bg-slate-200'></div>
-      <div className='w-[80px] h-[87px] bg-slate-200'></div>
-      <div className='w-[80px] h-[80px] bg-slate-200'></div>
+    <div className='flex items-start justify-center gap-2 mt-6'>
+      <div className='w-[80px] h-[87px] bg-[#3E3E3E]'></div>
+      <div className='w-[80px] h-[80px] bg-[#3E3E3E]'></div>
+      <div className='w-[80px] h-[87px] bg-[#3E3E3E]'></div>
+      <div className='w-[80px] h-[80px] bg-[#3E3E3E]'></div>
     </div>
   )
 }
