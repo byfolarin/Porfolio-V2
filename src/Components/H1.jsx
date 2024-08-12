1bfc364652078ad7304f68f09ed3942223713d14
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-12 09:15:02 +0100

    flex

--- src/Components/H1.jsx
+++ src/Components/H1.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const H1 = () => {
   return (
     <div className="h-screen flex items-center justify-end">
+      <p className='font-mono text-[white]'>PRODUCT</p>
       <div className=" w-full flex  justify-end px-4"> {/* Added full width and horizontal padding */}
-        <h1 className='text-[#F13C00] text-[190px] tracking-[-0.11em] leading-11'> 
+        <h1 className='text-[#F13C00] text-[350px] tracking-[-0.03em] leading-11'> 
           ENGINEER
         </h1>
       </div>
