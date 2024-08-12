267eb3ac167225b6a39a6a1a6a25cf10ffc8ba97
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-12 13:21:01 +0100

    h1

--- src/Components/H1.jsx
+++ src/Components/H1.jsx
@@ -4,12 +4,12 @@ const H1 = () => {
   return (
     <div className="h-screen flex items-center justify-end">
 
-      <p className='font-mono text-[white] mb-[460px] '>FRONT-END</p>
+      <p className='font-mono text-[white] mb-[460px] '>PRODUCT</p>
 
       <div className=" w-full flex justify-center lg:justify-end"> {/* Added full width and horizontal padding */}
 
-        <h1 className='text-[#F13C00] text-[128px]  lg:text-[350px] tracking-[-0.05em] leading-11 lg:pb-[300px] lg:pr-4'> 
-          ENGINEER
+        <h1 className='text-[#434343] text-[128px]  lg:text-[350px] tracking-[-0.05em] leading-11 lg:pb-[300px] lg:pr-4'> 
+          DESIGNER
         </h1>
 
       </div>
