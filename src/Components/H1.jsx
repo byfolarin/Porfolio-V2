8e1baa068003eb2319ee6009a249c7247a22d91f
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-12 07:09:17 +0100

    sibs

--- src/Components/H1.jsx
+++ src/Components/H1.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 const H1 = () => {
   return (
-    <div className="h-screen flex items-end justify-center overflow-hidden">
-      <div className="mb-8 w-full px-4"> {/* Added full width and horizontal padding */}
-        <h1 className='text-[#F13C00] text-[15vw] 
-        sm:text-[20vw] md:text-[25vw] lg:text-[280px] 
-        tracking-[-0.12em] leading-[0.8] text-center
-        break-words'> {/* Added text-center and break-words */}
-          ENGINEER
+    <div className="h-screen flex items-end justify-center">
+      <div className="mb-8 w-full flex  justify-center px-4"> {/* Added full width and horizontal padding */}
+        <h1 className='text-[#F13C00] font-Bebas text-[280px] 
+        tracking-[-0.12em] leading-[0.8]'> 
+          DESIGN
         </h1>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'Bebas': ['BebasNeue-Regular', 'sans-serif'],
+    },
   },
   plugins: [],
 }
