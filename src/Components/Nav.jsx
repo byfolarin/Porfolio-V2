10fa239a59a6412c73e373447f9cff50de75c1cd
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-12 10:33:00 +0100

    plans

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,26 +2,25 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='text-[white] flex justify-between mt-8 '>
-      <div>
-        <h2 className='font-mono'>FOLARIN ABIOLA</h2>
-        <h2 className='font-mono text-[#F13C00] cursor-pointer'>ABOUT</h2>
-      </div>
+    <div className='text-[white] flex justify-between mt-3 '>
 
-      <div className=' lg:flex-row '>
+        <div>
+          <h2 className='font-mono'>FOLARIN ABIOLA</h2>
+          <h2 className='font-mono text-[#F13C00] cursor-pointer'>ABOUT</h2>
+        </div>
 
-      <div className='md:flex-row lg:flex-row lg:gap-2'>
 
-            <h2 className='font-mono'>OPEN TO WORK</h2>
-            <p className='hidden lg:block'>-</p>
-            <h2 className='text-[#F13C00] font-mono'> TOMORROW</h2>
-      </div>
+        <div className='flex lg:flex-row lg:justify-between gap-2'>
 
-      <div>
-        <h2 className='font-mono cursor-pointer'>CONTACT</h2>
-      </div>
+              <h2 className='font-mono'>OPEN TO WORK </h2>
+              <p className='hidden lg:block'>-</p>
+              <h2 className='text-[#F13C00] font-mono'> TOMORROW</h2>
+        </div>
 
-      </div>
+        <div>
+          <h2 className='font-mono cursor-pointer'>CONTACT</h2>
+        </div>
+
     </div>
   )
 }
