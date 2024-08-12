7cf726280c7625d944f5edf7227341310a4696a2
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-12 13:08:17 +0100

    img

--- src/Components/ImgChange.jsx
+++ src/Components/ImgChange.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Project from './Project'
 import { motion } from "framer-motion"
+import Proj1 from './ProjectImg/Proj1.jpeg'
+import Proj2 from './ProjectImg/Proj2.jpeg'
+import Proj3 from './ProjectImg/Proj3.jpeg'
+import Proj4 from './ProjectImg/Proj4.jpeg'
+import Proj5 from './ProjectImg/Proj4.jpeg'
 
 const ImgChange = () => {
   return (
@@ -12,11 +17,26 @@ const ImgChange = () => {
 
 
       <div  className='flex items-start justify-center gap-2 mt-6 cursor-pointer'>
-            <motion.div className='w-[80px] h-[87px] bg-[#3E3E3E]'></motion.div>
-            <div className='w-[80px] h-[80px] bg-[#3E3E3E]'></div>
-            <div className='w-[80px] h-[87px] bg-[#3E3E3E]'></div>
-            <div className='w-[80px] h-[80px] bg-[#3E3E3E]'></div>
-            <div className='w-[80px] h-[87px] bg-[#3E3E3E]'></div>
+
+            <div className='w-[80px] h-[87px] bg-[#3E3E3E]'>
+               <img src={Proj2} alt="" className='object-cover w-full h-full'/>
+            </div>
+
+            <div className='w-[80px] h-[80px] bg-[#3E3E3E]'>
+                <img src={Proj1} alt="" className='object-cover w-full h-full'/>
+            </div>
+
+            <div className='w-[80px] h-[87px] bg-[#3E3E3E]'>
+                    <img src={Proj3} alt="" className='object-cover w-full h-full' />
+            </div>
+
+            <div className='w-[80px] h-[80px] bg-[#3E3E3E]'>
+             <img src={Proj4} alt="" className='object-cover w-full h-full' />
+            </div>
+
+            <div className='w-[80px] h-[87px] bg-[#3E3E3E]'>
+            <img src={Proj5} alt="" className='object-cover w-full h-full' />
+            </div>
             <div className='w-[80px] h-[80px] bg-[#3E3E3E]'></div>
       </div>
 
