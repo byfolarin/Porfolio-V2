ae7c1609e44f0ac0911888252280ed20716c45bc
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-21 02:26:38 +0100

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import './App.css'
-import H1 from './Components/H1'
-import Nav from './Components/Nav'
-import ImgChange from './Components/ImgChange'
+// import H1 from './Components/H1'
+// import Nav from './Components/Nav'
+// import ImgChange from './Components/ImgChange'
+import PricingCards from './Components/PricingCards'
 
 function App() {
   return (
     <div className="h-screen flex flex-col justify-between mx-2 lg:mx-11 overflow-hidden">
 
+      <PricingCards />
+{/* 
         <Nav/>
           <div className="flex-grow">
             <ImgChange />
           </div>
-        <H1/>
+        <H1/> */}
 
     </div>
   )
---
+++ src/Components/PricingCards.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const PricingCards = () => {
+
+    const pricingdetails = [
+        { title: "Project Completed", description: "Over 200 successful projects completed, showcasing our extensive experience and portfolio.", icon: densityMediumSvg },
+        { title: "Interior Design", description: "Transforming spaces with innovative and personalized interior design solutions.", icon: linkedServicesSvg },
+        { title: "Space Survey", description: "Comprehensive space analysis to optimize functionality and aesthetics.", icon: radioButtonUncheckedSvg },
+        { title: "3D Visualization", description: "Bringing ideas to life with stunning 3D renderings and walkthroughs.", icon: fullscreenSvg },
+        { title: "Renovation", description: "Expertly updating and modernizing existing spaces for enhanced living and working.", icon: filterSvg },
+        { title: "Consultation", description: "Professional advice and guidance for all your design and architectural needs.", icon: dynamicFormSvg }
+      ]
+
+  return (
+    <div className='text-black'>
+             Hello
+    </div>
+  )
+}
+
+export default PricingCards
